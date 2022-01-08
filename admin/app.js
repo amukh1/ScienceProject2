74e1fca597bcfae157ba742d1a02801569085612
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-01-08 11:14:17 -0500

    aa

---
+++ admin/app.js
@@ -0,0 +1,60 @@
+function epik(e) {
+    // console.log(e.style.backgroundColor)
+  if(e.style.backgroundColor == 'rgb(219, 255, 87)'){
+    del(e)
+  }else{
+    e.style.backgroundColor = 'rgb(219, 255, 87)'
+    e.style.color = 'black'
+  }
+
+}
+
+function del(i) {
+    i.style.opacity = '0'
+    ltr(i)
+}
+
+
+function ltr(e) {
+    e.style.display = 'none'
+}
+
+var intervalID = setInterval(myCallback, 500);
+let lf = null
+function myCallback(){
+    var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+         var data = JSON.parse(xhr.responseText)
+     console.log(data)
+     if(data.fact == lf){
+            return
+     }else {
+         lf = data.fact
+     let div = document.createElement('div');
+     div.classList.add('post');
+     div.setAttribute("id", "post");
+      div.setAttribute("onClick", "epik(this)")
+      document.getElementById('content').appendChild(div)
+      let fact = document.createElement('div');
+      fact.classList.add('fact')
+      fact.innerHTML = data.fact
+      div.appendChild(fact)
+        let author = document.createElement('div');
+        author.classList.add('name')
+        author.innerHTML = data.user
+        div.appendChild(author)
+    }}
+};
+xhr.open("GET", "https://aaa.amukh1.dev/fact", true);
+xhr.send();
+}
+
+
+
+
+
+function clear() {
+    window.location.reload();
+}
--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+function goPLS() {
+    fetch(`https://aaa.amukh1.dev/newFact?fact=${document.getElementById("fact").value}&name=${document.getElementById("name").value}`)
+    .then(response => response.json())
+    .then(data => {
+data = JSON.parse(data);
+console.log(data)
+    })
+    document.getElementById("cri").innerHTML = "Submitted!";
+    document.getElementById("cri").style.color = "rgb(30, 255, 0)";
+    let mt = setTimeout(backk, 2000);
+}
+
+
+function backk() {
+    document.getElementById("cri").innerHTML = "Science Fact!";
+    document.getElementById("cri").style.color = "white";
+}
